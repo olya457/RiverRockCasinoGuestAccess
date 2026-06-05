3e55f9342e13cf5a2095a235e207002f03f0aa6a
olya <olya0665643972@gmail.com>
2026-06-05 11:20:57 +0300

    init

--- android/app/src/main/java/com/riverrock/casinoguest/access/MainActivity.kt
+++ android/app/src/main/java/com/riverrock/casinoguest/access/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.riverrockcasinoguestaccess
+package com.riverrock.casinoguest.access
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
--- android/app/src/main/java/com/riverrock/casinoguest/access/MainApplication.kt
+++ android/app/src/main/java/com/riverrock/casinoguest/access/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.riverrockcasinoguestaccess
+package com.riverrock.casinoguest.access
 
 import android.app.Application
 import com.facebook.react.PackageList
